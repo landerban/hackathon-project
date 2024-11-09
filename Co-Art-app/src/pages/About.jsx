885fe161eb002bc99207f2f750fdcf509527502e
Eggzivine <waiyanmoemyintlwin31@gmail.com>
2024-11-10 03:18:56 +0900

    Added About us basic

--- Co-Art-app/src/pages/About.jsx
+++ Co-Art-app/src/pages/About.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import '../css/About.css'; // Import CSS
+import backgroundImg from '../assets/About_Us_bg'; // Import the single background image
 
 const About = () => {
   return (
