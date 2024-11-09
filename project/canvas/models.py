f060fc9652268ef9c67ecc2bb0b85c5d0208b291
leapin-lizards <wonjaechoi2@gmail.com>
2024-11-09 15:24:45 +0900

    time limit verification

--- project/canvas/models.py
+++ project/canvas/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-
+from django.conf import settings
 class Pixels(models.Model):
     time=models.DateTimeField(auto_now_add=True)
     canvas_id=models.IntegerField()
     pixel_x=models.IntegerField()
     piexl_y=models.IntegerField()
-    #placed_by=
+    placed_by=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    
 
     # hex color
     pixel_color=models.CharField(max_length=7, default="#ffffff")
--- project/canvas/views.py
+++ project/canvas/views.py
@@ -2,7 +2,10 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Pixels
+from django.contrib.auth import get_user_model
 from .serializer import PixelsSerializer
+from django.utils.timezone import now
+from datetime import datetime, timedelta
 
 @api_view(['GET'])
 def get_placements(request):
@@ -14,6 +17,15 @@ def get_placements(request):
 def place(request):
     serializer=PixelsSerializer(data=request.data)
     if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data,status=status.HTTP_201_CREATED)
+        placementtime=now()
+        placed_by=serializer.data['placed_by']
+        user_last_placed=getattr(get_user_model().objects().get(id=placed_by),'last_pixel_time')
+        user_time_limit=getattr(get_user_model().objects().get(id=placed_by),'time_limit_sec')
+        get_user_model().objects.filter(id=placed_by).update(user_last_placed=now())
+
+
+        if placementtime>=user_last_placed+timedelta(seconds=user_time_limit):
+            serializer.save()
+
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
     return Response(serializer.data,status=status.HTTP_400_BAD_REQUEST)
