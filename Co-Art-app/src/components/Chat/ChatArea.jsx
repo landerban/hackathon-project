ff9c74c07eff27fda2bfbe5540ed8a1ab75c0579
Kabel <jh0623.kim@gmail.com>
2024-11-10 06:19:45 +0900

    chat

--- Co-Art-app/src/components/Chat/ChatArea.jsx
+++ Co-Art-app/src/components/Chat/ChatArea.jsx
@@ -1,26 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "../../css/Chat/ChatArea.css";
 
 function ChatArea({ isDark }) {
-  const messages = [
-    { id: 1, sender: "User", text: "Hello, how are you?" },
-    { id: 2, sender: "Friend", text: "I'm good, thanks! How about you?" },
-    { id: 3, sender: "User", text: "Doing well, just working on a project." },
-    // Add more messages as needed
-  ];
+  const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const getTodayDate = () => {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, "0");
+    const day = String(today.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  };
+
+  useEffect(() => {
+    const fetchMessages = async () => {
+      const todayDate = getTodayDate();
+      try {
+        const response = await axios.get(
+          `http://127.0.0.1:8000/chat/2024-11-09`
+        );
+        if (Array.isArray(response.data)) {
+          setMessages(response.data);
+        } else {
+          console.error("Data is not an array:", response.data);
+        }
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching messages:", error);
+        setError("Error fetching messages");
+        setLoading(false);
+      }
+    };
+    fetchMessages();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>{error}</div>;
+  }
 
   return (
     <div className={`${isDark ? "dark" : ""} chat-area p-3`}>
-      {messages.map((message) => (
-        <div
-          key={message.id}
-          className={`message mb-2 p-2 rounded ${
-            message.sender === "User" ? "sent  ms-auto" : "received me-auto"
-          }`}
-        >
-          <div className="message-text">{message.text}</div>
-        </div>
-      ))}
+      {messages.length > 0 ? (
+        messages.map((message, index) => (
+          <div
+            key={index}
+            className={`message mb-2 p-2 rounded ${
+              message.author_name === "User"
+                ? "sent ms-auto"
+                : "received me-auto"
+            }`}
+          >
+            <div className="message-text">{message.message}</div>
+          </div>
+        ))
+      ) : (
+        <div>No messages</div>
+      )}
     </div>
   );
 }
