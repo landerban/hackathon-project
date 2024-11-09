1057791ede4314889abf4122d6a936fc21192463
leapin-lizards <wonjaechoi2@gmail.com>
2024-11-09 14:26:39 +0900

    added placements model and part of the api

---
+++ project/canvas/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.static import serve
+from rest_framework import routers
+from . import views
+appname="canvas"
+urlpatterns = [
+    path("api/get_placements", views.get_placements, name="get_placements"),
+]
--- project/canvas/views.py
+++ project/canvas/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Pixels
+from .serializer import PixelsSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def get_placements(request):
+    pixels = Pixels.objects.all()
+    serializer = PixelsSerializer(pixels, many=True)
+    return Response(serializer.data)
